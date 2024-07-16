e4362f03024e9590f61542f5ade39a720ca06814
Amit Agam <amit.a@sealsecurity.io>
2024-07-16 18:18:08 +0300

    fix test

--- tests/test_path.py
+++ tests/test_path.py
@@ -1,18 +1,11 @@
-
 import io
 import zipfile
 
-import pytest
-
-def test_getinfo_missing(self, alpharep):
-    zipfile.Path(alpharep)
-    with self.assertRaises(KeyError):
-        alpharep.getinfo('does-not-exist')
 
 def test_malformed_paths():
-    '''
+    """
     Path should handle malformed paths.
-    '''
+    """
     data = io.BytesIO()
     zf = zipfile.ZipFile(data, "w")
     zf.writestr("/one-slash.txt", b"content")
